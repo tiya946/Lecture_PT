2225eb04efe6a6680194260a2b42b0b718f75123
tiya946 <tiya090406@gmail.com>
2025-07-10 10:45:19 +0530

    changes

---
+++ Prog2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture_PT
+{
+    internal class Prog2
+    {
+        public static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 5;
+            int res = a++ + b++;
+            //res = a;
+            //a = a + 1;
+            Console.WriteLine(res);
+        }
+    }
+}
